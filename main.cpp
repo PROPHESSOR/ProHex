c62547bafc685840255102f9dde6d2cc31d3a354
PROPHESSOR <xenteze@gmail.com>
2018-12-16 02:24:37 +0200

    Добавил реагирование на -v и -h (+ --)

--- main.cpp
+++ main.cpp
@@ -2,8 +2,25 @@
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
+    if(argc == 2) {
+        QString arg(argv[1]);
+
+        if(arg == "-v" || arg == "--version") {
+            qDebug() << "" + PROHEX + " v." + PROHEX_VERSION;
+            qDebug() << "Copyright (c) PROPHESSOR 2018";
+            return 0;
+        } else if(arg == "-h" || arg == "--help") {
+            qDebug() << "" + PROHEX + " v." + PROHEX_VERSION;
+            qDebug() << "Copyright (c) PROPHESSOR 2018";
+            qDebug() << "=============================";
+            qDebug() << "You can get help using F1 key in program!";
+            return 0;
+        }
+    }
+
     QApplication a(argc, argv);
     MainWindow w;
+
     w.show();
 
     return a.exec();
